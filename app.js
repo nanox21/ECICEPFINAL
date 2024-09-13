2d1c5441270b3d5647ad1ea7689eac2355cec529
nanox21 <160341514+nanox21@users.noreply.github.com>
2024-09-13 11:28:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 window.onload = function () {
-    var mensaje = "Esta calculadora no reemplaza el criterio clínico y su uso es de responsabilidad del operador. Si está de acuerdo, presione 'Continuar'.";
-    if (confirm(mensaje)) {
-        alert("Has aceptado. Puedes continuar.");
-    } else {
-        alert("Has cancelado. No puedes continuar.");
-        window.location.href = "https://www.google.com";
+    // Comprobar si la alerta ya se mostró anteriormente
+    if (!localStorage.getItem('alertaMostrada')) {
+        var mensaje = "Esta calculadora no reemplaza el criterio clínico y su uso es de responsabilidad del operador. Si está de acuerdo, presione 'Continuar'.";
+        
+        if (confirm(mensaje)) {
+            alert("Has aceptado. Puedes continuar.");
+            // Guardar en localStorage que la alerta ya se mostró
+            localStorage.setItem('alertaMostrada', 'true');
+        } else {
+            alert("Has cancelado. No puedes continuar.");
+            window.location.href = "https://www.google.com";
+        }
     }
 };
 
